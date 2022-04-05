14f7332b86e1d879f8e197ad3e297bb3f29f45e3
Juliano Teodoro <julianoteodoro2002@gmail.com>
2022-04-05 11:33:49 -0300

    Vuex

---
+++ vue-x/src/components/Contador.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2 class="font-weight-light">Contador: {{ contador }}</h2>
+        <button class="btn btn-lg btn-danger mr-1" @click="decrementar">-</button>
+        <button class="btn btn-lg btn-success" @click="incrementar">+</button>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+      contador(){
+          return this.$store.state.contador
+      }
+    },
+    methods: {
+        incrementar(){
+            this.$store.state.contador++;
+        },
+        decrementar(){
+            this.$store.state.contador--;
+        }
+    },
+}
+</script>
