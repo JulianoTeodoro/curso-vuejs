bed46bdbf47e938acbcd778740c01c6d0b908ae4
Juliano Teodoro <julianoteodoro2002@gmail.com>
2022-04-05 11:55:41 -0300

    Vuex

--- vue-x/src/components/Contador.vue
+++ vue-x/src/components/Contador.vue
@@ -1,17 +1,29 @@
 <template>
     <div>
         <h2 class="font-weight-light">Contador: {{ contador }}</h2>
+        <h2 class="font-weight-light">Contador Multiplicado: {{ contadorMultiplicado }}</h2>
+
         <button class="btn btn-lg btn-danger mr-1" @click="decrementar">-</button>
         <button class="btn btn-lg btn-success" @click="incrementar">+</button>
     </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
-    computed: {
-      contador(){
-          return this.$store.state.contador
-      }
+    data(){
+        return{
+            contadorLocal: 2
+        }
+    },
+    computed: {  
+        ...mapState({
+        contador: state => state.contador,
+        contadorMultiplicado(state){
+            return state.contador * this.contadorLocal
+        }
+    }) 
     },
     methods: {
         incrementar(){
